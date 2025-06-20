f0b55445df35fd722a54558ce14d57775709e90c
warrre <warren.soulsey@gmail.com>
2025-06-20 18:44:26 +0200

    Refactor user API implementation; add user model and CRUD operations in facade

--- part 2/app/__init__.py
+++ part 2/app/__init__.py
@@ -1,25 +1,19 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import ns as user_ns
-from app.api.v1.places import ns as place_ns
-from app.api.v1.reviews import ns as review_ns
-from app.api.v1.amenities import ns as amenity_ns
-
+from app.api.v1.users import api as users_ns
 
 def create_app():
     app = Flask(__name__)
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        doc='/api/v1/'  # Swagger documentation endpoint
-    )
-    
-    api.add_namespace(user_ns, path='/api/v1/users')
-    api.add_namespace(place_ns, path='/api/v1/places')
-    api.add_namespace(review_ns, path='/api/v1/reviews')
-    api.add_namespace(amenity_ns, path='/api/v1/amenities')
-    # Les namespaces seront ajoutés ici plus tard
+    api = Api(app, version='1.0', title='HBnB API', description='API de l’application HBnB')
+    app,
+    version='1.0',
+    title='HBnB API',
+    description='HBnB Application API',
+    doc='/api/v1/'  # Swagger documentation endpoint
 
-    return app
+
+    # Les namespaces seront ajoutés ici plus tard
+    
+    api.add_namespace(users_ns, path='/api/v1/users')
+
+return app
--- part 2/app/services/facade.py
+++ part 2/app/services/facade.py
@@ -3,14 +3,67 @@ from app.persistence.repository import InMemoryRepository
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Placeholder pour la création d’un utilisateur
     def create_user(self, user_data):
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder pour récupérer un lieu par ID
-    def get_place(self, place_id):
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        """Retourne tous les utilisateurs sans le mot de passe"""
+        users = self.user_repo.all()
+        return [
+            {
+                'id': u.id,
+                'first_name': u.first_name,
+                'last_name': u.last_name,
+                'email': u.email
+            }
+            for u in users
+        ]
+
+    def update_user(self, user_id, data):
+        """Met à jour un utilisateur"""
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+
+        # Mettre à jour les champs autorisés
+        for field in ['first_name', 'last_name', 'email', 'password']:
+            if field in data:
+                setattr(user, field, data[field])
+
+        self.user_repo.update(user_id, user)
+
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        }
+    
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return [vars(a) for a in self.amenity_repo.all()]
+
+    def update_amenity(self, amenity_id, data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        if 'name' in data:
+            amenity.name = data['name']
+        self.amenity_repo.update(amenity_id, amenity)
+        return vars(amenity)
