107125121afa188a0962b1e5c7f8121cd501c93b
nawfel83 <laklitnawfel1@gmail.com>
2025-07-07 02:29:08 +0200

    Refactor app initialization: streamline create_app function and remove unused API namespaces

--- part 3/app/__init__.py
+++ part 3/app/__init__.py
@@ -1,23 +1,15 @@
 from flask import Flask
-from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.reviews import api as reviews_ns
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from config import DevelopmentConfig
 
-def create_app():
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='API de l’application HBnB',
-        doc='/api/v1/'
-    )
-
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-
+    app.config.from_object(config_class)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    # ... importer et enregistrer les blueprints/namespaces ici ...
     return app
