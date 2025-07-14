453606317c3cf10466dff70eff961400223bd9bf
nawfel83 <laklitnawfel1@gmail.com>
2025-07-14 22:08:13 +0200

    Refactor associations.py: correct comment formatting and improve readability of place_amenity table definition

--- part 3/app/models/associations.py
+++ part 3/app/models/associations.py
@@ -1,9 +1,8 @@
-from app import db
 from sqlalchemy import Table, Column, String, ForeignKey
+from app import db
 
-# Table d'association pour la relation many-to-many Place-Amenity
-place_amenity = Table
-(
+# Table d'association pour la relation Many-to-Many Place <-> Amenity
+place_amenity = Table(
     'place_amenity',
     db.Model.metadata,
     Column('place_id', String(36), ForeignKey('places.id'), primary_key=True),
